4fab77ca59ae03c7375005a2638c156edcb49869
Marcos Nicolau <marcosnicolau2003@gmail.com>
2021-04-03 13:01:31 -0300

    Service workers

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorkerRegistration.register();
+
+if ("serviceWorker" in navigator) {
+	window.addEventListener("load", function () {
+		serviceWorkerRegistration.register();
+	});
+}
