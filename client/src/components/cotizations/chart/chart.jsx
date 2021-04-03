2c1376a82cef18b44924deb82b200b469a104ee1
Marcos Nicolau <marcosnicolau2003@gmail.com>
2021-04-03 16:18:53 -0300

    Cache

--- client/src/components/cotizations/chart/chart.jsx
+++ client/src/components/cotizations/chart/chart.jsx
@@ -23,6 +23,7 @@ const CryptoChart = () => {
 
 	//Set a default value for the datapoint
 	useEffect(() => {
+		if (!prices) return;
 		datapoint.current.innerText = prices.length ? `$ ${prices[prices.length - 1]}` : "-";
 	}, [prices, datapoint.currentt]);
 
--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+    app.use(express.static(path.join(__dirname, '..', 'client', 'build'), { maxAge: '31536000' }));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
     });
