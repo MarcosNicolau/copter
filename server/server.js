16c784d998314be1346ac90d4627bfbab58aa5f7
Marcos Nicolau <marcosnicolau2003@gmail.com>
2021-04-03 16:23:42 -0300

    Cache year

--- server/server.js
+++ server/server.js
@@ -14,7 +14,8 @@ app.use(bodyParser.json());
 app.use(bodyParser({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '..', 'client', 'build'), { maxAge: '31536000' }));
+    res.set('Cache-control', 'public, max-age=31536000');
+    app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
     });
