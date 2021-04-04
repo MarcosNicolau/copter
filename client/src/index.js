913e5cc35c3c5a99db5f23e997ed4ac59b200bd0
Marcos Nicolau <marcosnicolau2003@gmail.com>
2021-04-04 16:46:54 -0300

    Strict

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
+);
 
 serviceWorkerRegistration.register();
 
