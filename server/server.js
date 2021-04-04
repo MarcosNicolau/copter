cba395bce73e8d85bdc19a2320c11382de9a53e3
Marcos Nicolau <marcosnicolau2003@gmail.com>
2021-04-04 17:10:04 -0300

    Production

--- server/server.js
+++ server/server.js
@@ -9,11 +9,12 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express({ extended: true }));
 
-if (process.env.NODE_ENV === 'PRODUCTION') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
