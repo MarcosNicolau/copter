fd8de977fe652a053fa3d01767e2688157a8f8a1
Marcos Nicolau <marcosnicolau2003@gmail.com>
2021-04-04 17:16:39 -0300

    Security policy

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(helmet());
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+    })
+);
 app.use(cors());
 app.use(express.json());
 app.use(express({ extended: true }));
