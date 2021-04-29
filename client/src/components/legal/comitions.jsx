037d8c10b68471029004431b88ee8770c337e7b0
Marcos <marcosnicolau.capo@gmail.com>
2021-04-29 14:06:24 -0300

    Comitions

--- client/src/components/legal/comitions.jsx
+++ client/src/components/legal/comitions.jsx
@@ -10,7 +10,7 @@ const Comitions = () => {
 			</ul>
 			<ul>
 				<h5>Trading:</h5>
-				<li>Mínimo para operar: 0.001 BTC o su equivalente</li>
+				<li>Mínimo para operar: 0.0001 BTC o su equivalente</li>
 				<li>Pares con ARS: 2%</li>
 				<li>Pares con cripto/cripto: 0.5%</li>
 				<li>Pares con USD: 1.37%</li>
